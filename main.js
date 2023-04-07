c43bc4bb1d9b52a6d0ec8b39e3b742c6059f2f01
Elmurod1215 <123142646+Elmurod1215@users.noreply.github.com>
2023-04-07 19:16:55 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log("salom")
+console.log("Men 1997-yil tug'ilganman")
+console.log("50")
+console.log(98)
+let sum=500
+let pul=prompt("summani kiriting")
+let (pul>=sum) ? "ha ":"yuq"
+console.log(pul)
+let son=10.5
+console.log(son)
+console.log(Math.round(Math.random()*10))
+console.log("salom")
+console.log(80)
+console.log("tur")
